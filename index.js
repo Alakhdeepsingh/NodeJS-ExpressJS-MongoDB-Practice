5bb2608acbf693c1e441917245e97a106f421a2f
Alakhdeep singh <54256747+Alakhdeepsingh@users.noreply.github.com>
2023-05-28 14:34:13 +0530

    nodejspractice

---
+++ index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+// const app = require('./app')
+// console.log(app);
+// // { x: 10, y: 20 }
+
+// console.log(app.x);
+// console.log(app.y);
+// // 10
+// // 20
+// console.log(app.z())
+console.log("10"); 
+fs.writeFileSync("hello.txt", "dry")
